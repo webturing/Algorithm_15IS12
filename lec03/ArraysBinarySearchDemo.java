7065e25dd52732a41a8b02292acbd4e8a6a97166
zj <zj@ahstu.cc>
2018-03-12 16:03:48 +0800

    lec03 binarySearch

---
+++ lec03/ArraysBinarySearchDemo.java
@@ -0,0 +1,17 @@
+package lec07;
+
+import java.util.Arrays;
+
+public class ArraysBinarySearchDemo {
+    public static void main(String[] args) {
+        int a[] = {1, 3, 5, 6, 6, 7, 10, 13, 25};
+        int key = 13;
+        int pos = Arrays.binarySearch(a, key);
+        if (pos < 0)
+            System.out.println("NOT FOUND!");
+        else
+            System.out.println(String.format("%d occured at pos=%d firstly.",
+                    key, pos));
+
+    }
+}
